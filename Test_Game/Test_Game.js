9b544c7bbd04f0c9eacc1cc05d82868e7b518d46
Mitten, Jeffrey <Jeffrey_Mitten2@cable.comcast.com>
2018-06-20 10:29:31 -0600

    fixed test game

--- Test_Game/Test_Game.js
+++ Test_Game/Test_Game.js
@@ -1,5 +1,5 @@
 var mappics = {};
-mappics[0] = { name : "Test.png", width : 1920, height : 1080 };
+mappics[0] = { name : "testgamemap.png", width : 1920, height : 1080 };
 
 var route = {};
 route[0] = { stepname: "Start!", note: "Test!", map: 0, scale: 1, mapX: 0, mapY: 0, arrowX: 272, arrowY: 197, rotation: 180, blockX: 122, blockY: 210 };
--- modules.js
+++ modules.js
@@ -1,3 +1,3 @@
 var modulelist = {};
 modulelist[0] = { name : "Super Metroid", folder : "Super_Metroid" };
-modulelist[1] = { name : "Test Me", folder : "Test" };
+modulelist[1] = { name : "Test Game", folder : "Test_Game" };
