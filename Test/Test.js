b981f2e642fefecf94c9a1a261d5f13ca1575b8b
Mitten, Jeffrey <Jeffrey_Mitten2@cable.comcast.com>
2018-06-19 18:28:15 -0600

    modulization

---
+++ Test/Test.js
@@ -0,0 +1,7 @@
+var mappics = {};
+mappics[0] = { name : "Test.png", width : 1920, height : 1080 };
+
+var route = {};
+route[0] = { stepname: "Start!", note: "Test!", map: 0, scale: 1, mapX: 0, mapY: 0, arrowX: 272, arrowY: 197, rotation: 180, blockX: 122, blockY: 210 };
+route[1] = { note: "Do stuff good", map: 0, scale: 0.5, mapX: 0, mapY: 0, arrowX: 550, arrowY: 389, rotation: 270, blockX: 164, blockY: 99 };
+route[2] = { stepname: "End!", map: 0, scale: 1, mapX: -100, mapY: -200, arrowX: 301, arrowY: 644, rotation: 0, blockX: 1031, blockY: 562 };
---
+++ modules.js
@@ -0,0 +1,3 @@
+var modulelist = {};
+modulelist[0] = { name : "Super Metroid", folder : "Super_Metroid" };
+modulelist[1] = { name : "Test Me", folder : "Test" };
