cf01008a54b34fb69e22d40ea4ef28a78bc39bb0
Mitten, Jeffrey <Jeffrey_Mitten2@cable.comcast.com>
2018-07-11 17:12:51 -0600

    updates to add lttp

---
+++ LttP/LttP.js
@@ -0,0 +1,26 @@
+var mappics = {};
+mappics[0] = { name : "lightworld.png", width : 2007, height : 2007 };
+mappics[1] = { name : "darkworld.png", width : 2007, height : 2007 };
+mappics[2] = { name : "uncle.png", width : 512, height : 512 };
+mappics[3] = { name : "escape.png", width : 1536, height : 3840 };
+
+var route = {};
+route[0] = { stepname: "Start!", note: "Wake up, skip the chest and head out", map: 0, scale: 1, mapX: -500, mapY: -800, arrowX: 636, arrowY: 17, rotation: 0, blockX: 588, blockY: 589 };
+route[1] = { note: "Talk to uncle to get a sword and shield", map: 2, scale: 1, mapX: 400, mapY: 100, arrowX: 461, arrowY: 560, rotation: 0, blockX: 805, blockY: 244 };
+route[2] = { map: 0, scale: 1, mapX: -400, mapY: -500, arrowX: 544, arrowY: 359, rotation: 0, blockX: 695, blockY: 368 };
+route[3] = { map: 3, scale: 0.5, mapX: 400, mapY: -900, arrowX: 555, arrowY: 125, rotation: 270, blockX: 571, blockY: 521 };
+route[4] = { note: "Kill blue guard for a small key", map: 3, scale: 1, mapX: -700, mapY: -1900, arrowX: 554, arrowY: 322, rotation: 90, blockX: 571, blockY: 232 };
+route[5] = { note: "Kill blue guard for a small key and get the boomerang from the chest", map: 3, scale: 1, mapX: -500, mapY: -2300, arrowX: 372, arrowY: 76, rotation: 90, blockX: 773, blockY: 65 };
+route[6] = { stepname: "Meet Zelda!", note: "Kill knight for the boss key, open chest for the lantern, and escort Zelda back out", map: 3, scale: 1, mapX: -300, mapY: -2000, arrowX: 763, arrowY: 133, rotation: 270, blockX: 546, blockY: 433 };
+route[7] = { map: 3, scale: 0.5, mapX: 100, mapY: -1800, arrowX: 665, arrowY: 111, rotation: 90, blockX: 584, blockY: 164 };
+route[8] = { map: 3, scale: 0.5, mapX: 400, mapY: -800, arrowX: 554, arrowY: 338, rotation: 90, blockX: 569, blockY: 168 };
+route[9] = { map: 3, scale: 0.5, mapX: 400, mapY: 200, arrowX: 616, arrowY: 103, rotation: 0, blockX: 573, blockY: 558 };
+route[10] = { map: 3, scale: 1, mapX: -300, mapY: 100, arrowX: 751, arrowY: 131, rotation: 0, blockX: 983, blockY: 158 };
+route[11] = { map: 3, scale: 1, mapX: 0, mapY: -3100, arrowX: 914, arrowY: 426, rotation: 180, blockX: 758, blockY: 682 };
+route[12] = { map: 3, scale: 1, mapX: 0, mapY: -3100, arrowX: 232, arrowY: 39, rotation: 270, blockX: 893, blockY: 455 };
+route[13] = { note: "Kill rat for a small key", map: 3, scale: 1, mapX: 200, mapY: -2400, arrowX: 228, arrowY: 503, rotation: 0, blockX: 446, blockY: 614 };
+route[14] = { map: 3, scale: 1, mapX: 200, mapY: -2300, arrowX: 515, arrowY: 34, rotation: 0, blockX: 293, blockY: 678 };
+route[15] = { note: "Pull the right side switch, sticking to the right side as you enter the room to avoid a dialogue box", map: 3, scale: 1, mapX: 400, mapY: 200, arrowX: 631, arrowY: 601, rotation: 90, blockX: 775, blockY: 283 };
+
+
+//route[999] = { stepname: "End!", note: "Time stops when you enter TriForce room", map: 0, scale: 1, mapX: -100, mapY: -200, arrowX: 301, arrowY: 644, rotation: 0, blockX: 1031, blockY: 562 };
--- modules.js
+++ modules.js
@@ -1,3 +1,4 @@
 var modulelist = {};
-modulelist[0] = { name : "Super Metroid", folder : "Super_Metroid" };
-modulelist[1] = { name : "Test Game", folder : "Test_Game" };
+modulelist[0] = { name : "Super Metroid - 100%", folder : "Super_Metroid" };
+modulelist[1] = { name : "Link to the Past - Any% NMG", folder : "LttP" };
+modulelist[2] = { name : "Test Game", folder : "Test_Game" };
